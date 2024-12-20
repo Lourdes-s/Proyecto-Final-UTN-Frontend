5ae0e1202893610d00d6fdcd187b9d857dcf3617
Lourdes.s <lou_santillan98@hotmail.com>
2024-12-20 17:55:17 -0300

    fix import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './Context/AuthContextx.jsx'
+import { AuthProvider } from './Context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
 	<BrowserRouter>
